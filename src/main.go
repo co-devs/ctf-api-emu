a22d9276bada33b545abe0216f0a06e891e0441c
Mike Devens <co-devs@users.noreply.github.com>
2025-02-13 05:56:35 +0000

    Update devcontainers file, move code into src folder

--- src/main.go
+++ src/main.go
@@ -10,8 +10,6 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
-
-
 var db *sql.DB
 
 func main() {
@@ -22,11 +20,11 @@ func main() {
 	// router.Use(middleware.ApiKeyAuthMiddleware)
 
 	router.GET("/heartbeat", handlers.GetHeartbeat)
-	router.GET("/endpoints",middleware.ApiKeyAuthMiddleware, handlers.GetEndpoints)
-	router.GET("/live_flags",middleware.ApiKeyAuthMiddleware, handlers.GetLiveFlags)
-	router.GET("/submissions",middleware.ApiKeyAuthMiddleware, handlers.GetSubmittedFlags)
-	router.POST("/submit",middleware.ApiKeyAuthMiddleware, handlers.PostFlag)
-	router.GET("/status",middleware.ApiKeyAuthMiddleware, handlers.GetStatus)
+	router.GET("/endpoints", middleware.ApiKeyAuthMiddleware, handlers.GetEndpoints)
+	router.GET("/live_flags", middleware.ApiKeyAuthMiddleware, handlers.GetLiveFlags)
+	router.GET("/submissions", middleware.ApiKeyAuthMiddleware, handlers.GetSubmittedFlags)
+	router.POST("/submit", middleware.ApiKeyAuthMiddleware, handlers.PostFlag)
+	router.GET("/status", middleware.ApiKeyAuthMiddleware, handlers.GetStatus)
 	router.GET("/all_submissions", middleware.IsAdminAuthMiddleware, handlers.GetAllFlagSubmissions)
 	router.GET("/secret", middleware.IsAdminAuthMiddleware, handlers.GetAPIKeys)
 
